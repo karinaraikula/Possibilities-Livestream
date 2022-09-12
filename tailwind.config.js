40bd2debf5260c568a3849f1cb202384c43018b2
saijailona <sailotem@gmail.com>
2022-09-12 20:05:46 +0300

    värit lisätty

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        "primary-dark": "##210916ff",
+        "primary-light": "#E5C7B4ff",
+        "primary-color": "#E27A6Fff",
+        "secondary-dark": "#DF8F79ff",
+      },
+      backgroundImage: {
+        header: "url('src/inspiskuva.jpg)",
+      },
+    },
   },
+
   plugins: [],
-}
+};
