6d5f8a45824bec1ca6fd5f3b10c4f6cdb01c77a2
karinaraikula <karina.raikula@metropolia.fi>
2022-09-29 21:43:09 +0300

    new nav

--- src/hamNav.js
+++ src/hamNav.js
@@ -1,8 +1,9 @@
-function hamNav() {
+const hamNav = () => {
+  console.log("hamNav");
   var x = document.getElementById("hamNav");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
-}
+};
