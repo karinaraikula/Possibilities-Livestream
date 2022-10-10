13862354ae9a66bd8e012a8e3253a0c8b6676bb1
saijailona <sailotem@gmail.com>
2022-10-10 13:11:07 +0300

    etusivun leiskaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,37 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+
+    fontFamily: {
+      "header": ['"Aboreto"'],
+      "body": ['"Libre Franklin"']
+    },
+  
     screens: {
       sm: "640px",
       md: "768px",
       lg: "1024px",
       xl: "1280px",
       "2xl": "1536px",
-    },
+    },    
     extend: {
       colors: {
-        "primary-dark": "#000000",
+        "primary-dark": "#1F2122",
+        "secondary-dark": "#364044",
         "primary-light": "#FFFFFF",
-        "secondary-dark": "#160F2E",
-        "secondary-light": "#f1f2f6",
-        "primary-color-light": "#F3DFF6",
-        "metropolia-orange": "#D36A0D",
+        "secondary-light": "rgb(232, 237, 240)",
+        "primary-middle": '#A3B3BC',
+        "metropolia-orange": "#D36AD",
+        "kansi": "#edf2f5",
       },
       backgroundImage: {
         header: "url('src/inspiskuva.jpg)",
-      },
+      },      
     },
+
   },
 
+
+  
   plugins: [],
 };
